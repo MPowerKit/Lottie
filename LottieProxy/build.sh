4dcaf1bbea8326e3ac919eb4648ea69d0eadd9c0
Alex Dobrynin <shuron1988@gmail.com>
2024-08-04 14:35:08 +0300

    updated build script

--- LottieProxy/build.sh
+++ LottieProxy/build.sh
@@ -9,7 +9,9 @@ xcodebuild archive -destination "generic/platform=iOS Simulator" -project Lottie
 xcodebuild archive -destination "generic/platform=macOS,vairant=Mac Catalyst" -project LottieProxy.xcodeproj -scheme LottieProxy -configuration Release -archivePath Output/Output-maccatalyst SKIP_INSTALL=NO ENABLE_BITCODE=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 echo "create xcframework"
-xcodebuild -create-xcframework -archive Output/Output-iphoneos.xcarchive -framework LottieProxy.framework -archive Output/Output-iphonesimulator.xcarchive -framework LottieProxy.framework -archive Output/Output-maccatalyst.xcarchive -framework LottieProxy.framework -output Output/LottieProxy.xcframework
+xcodebuild -create-xcframework -archive Output/Output-iphoneos.xcarchive -framework LottieProxy.framework -archive Output/Output-iphonesimulator.xcarchive -framework LottieProxy.framework -output Output/LottieProxy-ios.xcframework
+
+xcodebuild -create-xcframework -archive Output/Output-maccatalyst.xcarchive -framework LottieProxy.framework -output Output/LottieProxy-maccatalyst.xcframework
 
 echo "sharpie bind"
-sharpie bind --sdk=iphoneos17.5 --output="Output" --namespace="Com.Airbnb.Lottie" --scope="Output/LottieProxy.xcframework/ios-arm64/LottieProxy.framework/Headers" "Output/LottieProxy.xcframework/ios-arm64/LottieProxy.framework/Headers/LottieProxy-Swift.h"
+sharpie bind --sdk=iphoneos17.5 --output="Output" --namespace="Com.Airbnb.Lottie" --scope="Output/LottieProxy-ios.xcframework/ios-arm64/LottieProxy.framework/Headers" "Output/LottieProxy-ios.xcframework/ios-arm64/LottieProxy.framework/Headers/LottieProxy-Swift.h"
