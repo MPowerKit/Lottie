6c3586269c5703448c4348682897700912736d5e
Alex Dobrynin <shuron1988@gmail.com>
2024-12-04 14:32:34 +0200

    added privacyinfo manifest to lottie proxie

--- LottieProxy/build.sh
+++ LottieProxy/build.sh
@@ -6,7 +6,7 @@ xcodebuild archive -destination "generic/platform=iOS" -project LottieProxy.xcod
 
 xcodebuild archive -destination "generic/platform=iOS Simulator" -project LottieProxy.xcodeproj -scheme LottieProxy -configuration Release -archivePath Output/Output-iphonesimulator SKIP_INSTALL=NO ENABLE_BITCODE=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -destination "generic/platform=macOS,vairant=Mac Catalyst" -project LottieProxy.xcodeproj -scheme LottieProxy -configuration Release -archivePath Output/Output-maccatalyst SKIP_INSTALL=NO ENABLE_BITCODE=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -destination "generic/platform=macOS,variant=Mac Catalyst" -project LottieProxy.xcodeproj -scheme LottieProxy -configuration Release -archivePath Output/Output-maccatalyst SKIP_INSTALL=NO ENABLE_BITCODE=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 echo "create xcframework"
 xcodebuild -create-xcframework -archive Output/Output-iphoneos.xcarchive -framework LottieProxy.framework -archive Output/Output-iphonesimulator.xcarchive -framework LottieProxy.framework -output Output/LottieProxy-ios.xcframework
@@ -14,4 +14,4 @@ xcodebuild -create-xcframework -archive Output/Output-iphoneos.xcarchive -framew
 xcodebuild -create-xcframework -archive Output/Output-maccatalyst.xcarchive -framework LottieProxy.framework -output Output/LottieProxy-maccatalyst.xcframework
 
 echo "sharpie bind"
-sharpie bind --sdk=iphoneos17.5 --output="Output" --namespace="Com.Airbnb.Lottie" --scope="Output/LottieProxy-ios.xcframework/ios-arm64/LottieProxy.framework/Headers" "Output/LottieProxy-ios.xcframework/ios-arm64/LottieProxy.framework/Headers/LottieProxy-Swift.h"
+sharpie bind --sdk=iphoneos18.1 --output="Output" --namespace="Com.Airbnb.Lottie" --scope="Output/LottieProxy-ios.xcframework/ios-arm64/LottieProxy.framework/Headers" "Output/LottieProxy-ios.xcframework/ios-arm64/LottieProxy.framework/Headers/LottieProxy-Swift.h"
